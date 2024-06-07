ad2924d3f38e6a7d7d17c0c5f57c713cdfc9bc0e
Mithia15 <mithiafattihamirdas@gmail.com>
2024-06-07 16:33:49 +0700

    menambahkan migration

---
+++ database/migrations/2024_06_07_092917_create_menu.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menu', function (Blueprint $table) {
+            $table->id();
+            $table->string('menu_name');
+
+            $table->unsignedBigInteger('mealtype_id');
+            $table->foreign('mealtype_id')->references('id')->on('mealtype');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menu');
+    }
+};
---
+++ database/migrations/2024_06_07_093059_create_transaksi_has_menu.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaksi_has_menu', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('transaksi_id');
+            $table->foreign('transaksi_id')->references('id')->on('transaksi');
+
+            $table->unsignedBigInteger('menu_id');
+            $table->foreign('menu_id')->references('id')->on('menu');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksi_has_menu');
+    }
+};
